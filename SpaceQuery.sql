2c6b56c466a41e9a6e0357155bbce31f8dbd67c5
Maz <maz.m.king@gmail.com>
2019-11-06 20:41:51 -0600

    updated query

--- SpaceQuery.sql
+++ SpaceQuery.sql
@@ -1,6 +1,6 @@
-SELECT u.id, u.date, u.city, u.country, l.id, l.vehicle, lo.location
+SELECT u.index, u.date, u.city, u.country, l.index, l.vehicle, lo.location
 FROM ufo_sightings as u
-INNER JOIN launches as l
+JOIN launches as l
 ON u.date = l.date
-INNER JOIN locations as lo
+JOIN locations as lo
 ON l.site = lo.site_id
