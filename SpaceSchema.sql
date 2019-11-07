2b3f9b45e7af1e186bc1dac060867a111faf2374
Maz <maz.m.king@gmail.com>
2019-11-06 20:36:41 -0600

    to sql added

--- SpaceSchema.sql
+++ SpaceSchema.sql
@@ -1,15 +1,13 @@
 CREATE TABLE ufo_sightings (
-  id INT PRIMARY KEY,
+  index INT PRIMARY KEY,
   date DATE,
   city TEXT,
   state TEXT,
-  country TEXT,
-  lat DECIMAL,
-  long DECIMAL
+  country TEXT
 );
 
 CREATE TABLE launches (
-  id TEXT PRIMARY KEY,
+  index TEXT PRIMARY KEY,
   date DATE,
   vehicle TEXT,
   site TEXT
@@ -18,4 +16,4 @@ CREATE TABLE launches (
 CREATE TABLE locations (
     site_id TEXT PRIMARY KEY,
     location TEXT
-)
+);
